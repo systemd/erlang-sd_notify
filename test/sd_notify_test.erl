f4d0613e53b0df0a0b5de1838a0c7d84ca7565da
Peter Lemenkov <lemenkov@redhat.com>
2017-04-12 12:40:39 +0200

    Move test message creation to where it belongs
    Signed-off-by: Peter Lemenkov <lemenkov@redhat.com>

--- test/sd_notify_test.erl
+++ test/sd_notify_test.erl
@@ -9,7 +9,6 @@ sd_notify_test_() ->
 sd_notify_test_local("19") ->
 	{ok, CWD} = file:get_cwd(),
 	FakeNotifyUnixSockName = CWD ++ "/fake-notify-udp-sock-" ++ integer_to_list(erlang:phash2(make_ref())),
-	TestMessage = integer_to_list(erlang:phash2(make_ref())),
 	{ok, FakeNotifyUnixSock} = gen_udp:open(0, [{ifaddr, {local, FakeNotifyUnixSockName}}, {active, false}, list]),
 	os:putenv("NOTIFY_SOCKET", FakeNotifyUnixSockName),
 
@@ -20,6 +19,7 @@ sd_notify_test_local("19") ->
 			{
 				"Try sending message",
 				fun() ->
+					TestMessage = integer_to_list(erlang:phash2(make_ref())),
 					sd_notify:sd_pid_notify_with_fds(0, 0, TestMessage, [1, 2, 3]),
 					{ok, {_Address, _Port, Packet}} = gen_udp:recv(FakeNotifyUnixSock, length(TestMessage), 1000),
 					?assertEqual(TestMessage, Packet)
